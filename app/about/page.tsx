78d8f466c08694324b360725f1f22ab5c096182c
mpinoh <alvingarrin@gmail.com>
2024-08-20 09:48:27 +0700

    Second update. The blog's dynamic route problem is not solved yet.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,17 +1,11 @@
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-    title: "About | Alvin Garrin",
-    description: "About me",
-  };
 
 const About = () => {
     return ( 
         <main>
             <div className="about">
                 <h2>About me</h2>
-                <p>Hi, I&lsquo;m Alvin Garrin, a 32 y/o web developer from Depok, Indonesia.</p>
-                <p>I&lsquo;m a web developer (and programming) enthusiast for over the last 22 months, working with TypeScript, HTML5, CSS3, React, and Next. I have built simple apps here like weather app, blog pages and BMI calculator. I might work with larger projects in the near future.</p>
+                <p>Hi, I&lsquo;m Alvin Garrin, a 33 y/o web developer from Depok, Indonesia.</p>
+                <p>I&lsquo;m a web developer (and programming) enthusiast for over 2 years, working with TypeScript, HTML5, CSS3, React.js, and Next.js. I have built simple apps here like weather app, blog pages and BMI calculator. I might work with larger projects in the near future.</p>
                 <p>I also have strong interest in Data Science. I use Python and Scikit-learn in this regard.</p>
                 <p>Aside from my recent interest in programming, I&lsquo;ve been freelancing as math/physics/chemistry private tutor for mid-high school students for over 9 years.</p>
                 <p>Graduated in 2014, I also hold Bachelor of Engineering in Materials Engineering from Bandung Institute of Technology.</p>
--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,14 +1,47 @@
-import type { Metadata } from "next";
+// app/blogs/page.tsx
 
-export const metadata: Metadata = {
-  title: "Blogs | Alvin Garrin",
-  description: "Blogs",
-};
-export default function Blogs() {
-    return (
-      <main >
-        <h1>blogs</h1>
-      </main>
-    );
+import Link from 'next/link';
+
+async function getBlogs() {
+  try {
+    // const res = await fetch('https://alvingarrin.vercel.app/blogs/api', { next: { revalidate: 1 } });
+    const res = await fetch('http://localhost:3000/blogs/api', { next: { revalidate: 1 } });
+    
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    return res.json();
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return [];
   }
-  
+}
+
+export default async function Blogs() {
+  const blogs = await getBlogs();
+
+  return (
+    <main>
+      <div>
+      {blogs.length === 0 ? (
+        <div className="error-message">
+          <p>Failed to fetch blogs. Please try again later.</p>
+        </div>
+      ) : (
+        <div className="blog-list">
+          <h2>Blogs</h2>
+          {blogs.map((blog: { id: string; title: string; author: string }) => (
+            <div className="blog-preview" key={blog.id}>
+              <Link href={`/blogs/${blog.id}`}>
+                <h2>{blog.title}</h2>
+                <p>Written by {blog.author}</p>
+              </Link>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+    </main>
+    
+  );
+}
