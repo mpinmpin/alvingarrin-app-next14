1f1dae1dea48c57df7614979a672269bf40e6b7b
alvingarrin <alvingarrin@gmail.com>
2025-02-17 10:17:11 +0700

    1.00 Update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,8 +5,8 @@ const About = () => {
 			<div className="about">
 				<h2>About me</h2>
 				<p>Hi, I&lsquo;m Alvin Garrin, a 33 y/o self-taught programmer and web developer from Depok, Indonesia.</p>
-				<p>I&lsquo;m a programming and web development enthusiast for over 2 years, working with TypeScript, HTML, CSS, and React/Next.js. I have built simple apps here like weather app, blog pages and BMI calculator. I might work with larger projects in the near future.</p>
-				<p>I also have strong interest in Data Science. I use Python and Scikit-learn in this regard.</p>
+				<p>I&lsquo;m a programming and web development enthusiast for over 2 years, working with TypeScript, HTML, CSS, and React/Next.js. I have built simple apps here like weather app, clicker minigame, blog pages and BMI calculator. I might work with larger projects in the near future.</p>
+				<p>I also have strong interest in Data Science. I use Python and Scikit-learn in this regard. I have received a certification in Intermediate Machine Learning from Kaggle.com.</p>
 				<p>Aside from my recent interest in programming, I&lsquo;ve been freelancing as math/physics/chemistry private tutor for mid-high school students for over 9 years.</p>
 				<p>Graduated in 2014, I also hold Bachelor of Engineering in Materials Engineering from Bandung Institute of Technology.</p>
 			</div>
